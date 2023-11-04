367d1dbea650704a5f22da73c6bdf05648853840
Patrick Roy <patroytall@gmail.com>
2023-11-04 12:06:16 -0400

    SASS theme

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 # Development environment
 
+Build the site:
+```bash
+deno task serve
+```
+Rebuild is required when adding new imports.
+
 Start development server on port 3000:
 ``` bash
 deno task serve
--- _config.ts
+++ _config.ts
@@ -2,9 +2,11 @@ import lume from "lume/mod.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import source_maps from "lume/plugins/source_maps.ts";
 import nav from "lume/plugins/nav.ts";
+import sass from "lume/plugins/sass.ts";
 
 const site = lume();
 
+site.use(sass());
 site.use(sitemap());
 site.use(source_maps());
 site.use(nav());
